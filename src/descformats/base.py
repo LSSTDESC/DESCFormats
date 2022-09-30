52952c637f2b988020877224f42df2551a66da6a
Eric Charles <echarles@gmail.com>
2022-09-29 22:33:21 -0700

    doing the do

--- src/descformats/base.py
+++ src/descformats/base.py
@@ -1,9 +1,16 @@
 import pathlib
+import shutil
+import warnings
+
+from ceci.handle import DataHandle
+import tables_io
+
 
 class FileValidationError(Exception):
     pass
 
-class DataFile:
+
+class DataFile(DataHandle):
     """
     A class representing a DataFile to be made by pipeline stages
     and passed on to subsequent ones.
@@ -15,27 +22,14 @@ class DataFile:
     to indicate what kind of file is expected.  The "suffix" attribute,
     which must be defined on subclasses, indicates the file suffix.
 
-    The open method, which can optionally be overridden, is used by the 
+    The open method, which can optionally be overridden, is used by the
     machinery of the PipelineStage class to open an input our output
     named by a tag.
 
     """
-    def __init__(self, path, mode, validate=True, **kwargs):
-        self.path = path
-        self.mode = mode
-        self.file = self.open(path, mode, **kwargs)
-        if validate and mode == 'r':
-            self.validate()
-
-    def validate(self):
-        """
-        Concrete subclasses should override this method
-        to check that all expected columns are present.
-        """
-        pass
 
     @classmethod
-    def open(cls, path, mode):
+    def _open(cls, path, mode, **kwargs):
         """
         Open a data file.  The base implementation of this function just
         opens and returns a standard python file object.
@@ -47,58 +41,89 @@ class DataFile:
         """
         return open(path, mode)
 
-    def close(self):
-        self.file.close()
+    @classmethod
+    def _close(cls, fileObj):
+        fileObj.close()
+
+
+
+class TableHandle(DataHandle):
+    """DataHandle for single tables of data
+    """
+    suffix = None
 
     @classmethod
-    def make_name(cls, tag):
-        if cls.suffix:
-            return f'{tag}.{cls.suffix}'
-        else:
-            return tag
+    def _open(cls, path, mode, **kwargs):
+        """Open and return the associated file
 
-class HDFFile(DataFile):
-    """
-    A data file in the HDF5 format.
+        Notes
+        -----
+        This will simply open the file and return a file-like object to the caller.
+        It will not read or cache the data
+        """
+        return tables_io.io.io_open(path, **kwargs)  #pylint: disable=no-member
+
+    @classmethod
+    def _read(cls, path, **kwargs):
+        """Read and return the data from the associated file """
+        return tables_io.read(path, **kwargs)
+
+    @classmethod
+    def _write(cls, data, path, **kwargs):
+        """Write the data to the associatied file """
+        return tables_io.write(data, path, **kwargs)
+
+    @classmethod
+    def _size(cls, path, **kwargs):
+        return tables_io.io.getInputDataLengthHdf5(path, **kwargs)
+
+    @classmethod
+    def _iterator(cls, path, **kwargs):
+        """Iterate over the data"""
+        return tables_io.iteratorNative(path, **kwargs)
+
+
+class Hdf5Handle(TableHandle):
+    """A data file in the HDF5 format.
     Using these files requires the h5py package, which in turn
     requires an HDF5 library installation.
-
     """
+
     suffix = 'hdf5'
     required_datasets = []
 
     @classmethod
-    def open(cls, path, mode, **kwargs):
+    def _open(cls, path, mode, **kwargs):
         # Suppress a warning that h5py always displays
         # on import
-        import warnings
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            import h5py
+            import h5py  # pylint: disable=import-outside-toplevel
         # Return an open h5py File
         return h5py.File(path, mode, **kwargs)
 
     def validate(self):
-        missing = [name for name in self.required_datasets if name not in self.file]
+        missing = [name for name in self.required_datasets if name not in self.fileObj]
         if missing:
             text = "\n".join(missing)
             raise FileValidationError(f"These data sets are missing from HDF file {self.path}:\n{text}")
 
-    def close(self):
-        self.file.close()
+    @classmethod
+    def _write_chunk(cls, data, fileObj, groups, start, end, **kwargs):
+        if groups is None:
+            tables_io.io.writeDictToHdf5ChunkSingle(fileObj, data, start, end, **kwargs)
+        else:  # pragma: no cover
+            tables_io.io.writeDictToHdf5Chunk(groups, data, start, end, **kwargs)
 
 
-class FitsFile(DataFile):
-    """
-    A data file in the FITS format.
-    Using these files requires the fitsio package.
-    """
+class FitsHandle(TableHandle):
+    """DataHandle for a table written to fits"""
     suffix = 'fits'
     required_columns = []
 
     @classmethod
-    def open(cls, path, mode, **kwargs):
-        import fitsio
+    def _open(cls, path, mode, **kwargs):
+        import fitsio   # pylint: disable=import-outside-toplevel
         # Fitsio doesn't have pure 'w' modes, just 'rw'.
         # Maybe we should check if the file already exists here?
         if mode == 'w':
@@ -110,11 +135,10 @@ class FitsFile(DataFile):
         Check that all supplied columns exist
         and are in the chosen HDU
         """
-        ext = self.file[hdu]
+        ext = self.fileObj[hdu]
         found_cols = ext.get_colnames()
         missing_columns = [col for col in columns if col not in found_cols]
         return missing_columns
-
 
     def validate(self):
         """Check that the catalog has all the required columns and complain otherwise"""
@@ -126,8 +150,20 @@ class FitsFile(DataFile):
             text = "\n".join(missing)
             raise FileValidationError(f"These columns are missing from FITS file {self.path}:\n{text}")
 
-    def close(self):
-        self.file.close()
+
+class PqHandle(TableHandle):
+    """DataHandle for a parquet table"""
+    suffix = 'pq'
+
+    @classmethod
+    def _close(cls, fileObj, **kwargs):
+        pass
+
+
+HDFFile = Hdf5Handle
+
+
+FitsFile = FitsHandle
 
 
 class TextFile(DataFile):
@@ -136,17 +172,19 @@ class TextFile(DataFile):
     """
     suffix = 'txt'
 
+
 class YamlFile(DataFile):
     """
     A data file in yaml format.
     """
     suffix = 'yml'
 
+
 class Directory(DataFile):
     suffix = ''
 
     @classmethod
-    def open(self, path, mode):
+    def _open(cls, path, mode, **kwargs):
         p = pathlib.Path(path)
 
         if mode == "w":
@@ -158,5 +196,6 @@ class Directory(DataFile):
                 raise ValueError(f"Directory input {path} does not exist")
         return p
 
-    def close(self):
+    @classmethod
+    def _close(cls, fileObj):
         pass
--- src/descformats/tx.py
+++ src/descformats/tx.py
@@ -1,16 +1,7 @@
-from ..base import FitsFile, HDFFile, DataFile, YamlFile
+import numpy as np
+from .base import FitsFile, HDFFile
+from .types import metacalibration_names
 
-def metacalibration_names(names):
-    """
-    Generate the metacalibrated variants of the inputs names,
-    that is, variants with _1p, _1m, _2p, and _2m on the end
-    of each name.
-    """
-    suffices = ['1p', '1m', '2p', '2m']
-    out = []
-    for name in names:
-        out += [name + '_' + s for s in suffices]
-    return out
 
 class MetacalCatalog(FitsFile):
     """
@@ -18,40 +9,18 @@ class MetacalCatalog(FitsFile):
     """
     # These are columns
     metacal_columns = [
-        'mcal_g', 'mcal_g_cov',  'mcal_pars',  'mcal_pars_cov', 
+        'mcal_g', 'mcal_g_cov',  'mcal_pars',  'mcal_pars_cov',
         'mcal_T', 'mcal_T_err', 'mcal_T_r', 'mcal_s2n_r',]
 
-    other_columns = ['mcal_flux_cov', 'mcal_weight', 'mcal_flux', 
+    other_columns = ['mcal_flux_cov', 'mcal_weight', 'mcal_flux',
         'mcal_flux_s2n', 'mcal_mag', 'mcal_gpsf', 'mcal_logsb', 'mcal_Tpsf']
 
     # The parent class will check these columns exist.
-    required_columns = ( metacal_columns 
-                        + metacalibration_names(metacal_columns) 
+    required_columns = ( metacal_columns
+                        + metacalibration_names(metacal_columns)
                         + other_columns )
 
     # Add methods for handling here ...
-
-
-class TomographyCatalog(HDFFile):
-    required_datasets = []
-
-    def read_zbins(self):
-        """
-        Read saved redshift bin edges from attributes
-        """
-        d = dict(self.file['tomography'].attrs)
-        tomo.close()
-        nbin = d['nbin']
-        zbins = [(d[f'zmin_{i}'], d[f'zmax_{i}']) for i in range(nbin)]
-        return zbins
-
-
-
-
-class RandomsCatalog(HDFFile):
-    required_datasets = ['randoms/ra', 'randoms/dec', 'randoms/e1', 'randoms/e2']
-
-
 
 
 class DiagnosticMaps(HDFFile):
@@ -62,8 +31,7 @@ class DiagnosticMaps(HDFFile):
 
     def read_healpix(self, map_name, return_all=False):
         import healpy
-        import numpy as np
-        group = self.file[f'maps/{map_name}']
+        group = self.fileObj[f'maps/{map_name}']
         nside = group.attrs['nside']
         npix = healpy.nside2npix(nside)
         m = np.repeat(healpy.UNSEEN, npix)
@@ -72,23 +40,19 @@ class DiagnosticMaps(HDFFile):
         m[pix] = val
         if return_all:
             return m, pix, nside
-        else:
-            return m
+        return m
 
     def display_healpix(self, map_name, **kwargs):
         import healpy
-        import numpy as np
         m, pix, nside = self.read_healpix(map_name, return_all=True)
         lon,lat=healpy.pix2ang(nside,pix,lonlat=True)
-        npix=healpy.nside2npix(nside)
         lon_range = [lon.min()-0.1, lon.max()+0.1]
         lat_range = [lat.min()-0.1, lat.max()+0.1]
         title = kwargs.pop('title', map_name)
         healpy.cartview(m,lonra=lon_range, latra=lat_range, title=title, **kwargs)
 
     def read_tangential(self, map_name):
-        import numpy as np
-        group = self.file[f'maps/{map_name}']
+        group = self.fileObj[f'maps/{map_name}']
         info = dict(group.attrs)
         nx = info['nx']
         ny = info['ny']
@@ -106,7 +70,7 @@ class DiagnosticMaps(HDFFile):
         m[y,x] = val
         ra_min, ra_max = info['ra_min'], info['ra_max']
         print(ra_min, ra_max)
-        if ra_min > 180 and ra_max<180:
+        if ra_min > 180 and ra_max < 180:
             ra_min -= 360
         ra_range = (ra_min, ra_max)
         dec_range = (info['dec_min'],info['dec_max'])
@@ -114,7 +78,6 @@ class DiagnosticMaps(HDFFile):
 
     def display_tangential(self, map_name, **kwargs):
         import pylab
-        import numpy as np
         m, ra_range, dec_range = self.read_tangential(map_name)
         extent = list(ra_range) + list(dec_range)
         title = kwargs.pop('title', map_name)
@@ -122,13 +85,3 @@ class DiagnosticMaps(HDFFile):
         pylab.title(title)
         pylab.colorbar()
         pylab.show()
-
-
-
-class PhotozPDFFile(HDFFile):
-    required_datasets = []
-
-
-class SACCFile(HDFFile):
-    suffix = 'sacc'
-
